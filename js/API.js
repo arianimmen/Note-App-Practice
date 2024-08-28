2f80f599c950b6e90f997419e9bf1db889667684
Arian Immen <Arianimmen6@gmail.com>
2024-08-28 16:22:27 +0330

    bug fixing

--- js/API.js
+++ js/API.js
@@ -3,7 +3,15 @@ export default class API {
     return JSON.parse(localStorage.getItem("notes")) || [];
   }
 
+  static getTheme() {
+    return JSON.parse(localStorage.getItem("theme")) || 0;
+  }
+
   static saveData(allData) {
     localStorage.setItem("notes", JSON.stringify(allData));
+  }
+
+  static saveTheme(theme) {
+    localStorage.setItem("theme", JSON.stringify(theme));
   }
 }
